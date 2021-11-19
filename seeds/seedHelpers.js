862bd8bacba361fd17d703d17edae3c5d0eb9128
Eric K <kummer1122@gmail.com>
2021-11-19 11:30:40 -0500

    initial Yelp Camp commit

---
+++ seeds/seedHelpers.js
@@ -0,0 +1,35 @@
+module.exports.descriptors = [
+	'Forest',
+	'Ancient',
+	'Petrified',
+	'Roaring',
+	'Cascade',
+	'Tumbling',
+	'Silent',
+	'Redwood',
+	'Maple',
+	'Misty',
+	'Ocean',
+	'Diamond',
+	'Dusty',
+	'Grizzly',
+	'Sky'
+];
+
+module.exports.places = [
+	'Flats',
+	'Village',
+	'Mountain',
+	'Valley',
+	'Park',
+	'Pond',
+	'Camp Ground',
+	'Camp',
+	'Country',
+	'Lake',
+	'River',
+	'Fields',
+	'Canyon',
+	'Ranch',
+	'Sands'
+];
