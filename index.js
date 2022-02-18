a5dc5831f1cb7312fe7ea99f4a5679a0fb7d5bba
Eric K <kummer1122@gmail.com>
2022-02-18 09:36:45 -0500

    aded error handling

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const Joi = require('joi');
 const ejsMate = require('ejs-mate');
 const catchAsync = require('./utils/catchAsync');
 const Campground = require('./models/campgrounds');
@@ -51,6 +52,21 @@ app.get('/campgrounds/new', (req, res) => {
 app.post(
 	'/campgrounds',
 	catchAsync(async (req, res, next) => {
+		const campgroundSchema = Joi.object({
+			campground: Joi.object({
+				title: Joi.string().required(),
+				price: Joi.number().required().min(0),
+				image: Joi.string().required(),
+				location: Joi.string().required(),
+				description: Joi.string().required()
+			}).required()
+		});
+		const { error } = campgroundSchema.validate(req.body);
+		if (error) {
+			const msg = error.details.map((el) => el.message).join(',');
+			throw new ExpressError(msg, 400);
+		}
+		console.log(result);
 		const campground = new Campground(req.body.campground);
 		await campground.save();
 		res.redirect(`/campgrounds/${campground._id}`);
